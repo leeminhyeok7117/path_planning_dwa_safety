e772ff678cae6edaae0e297fd9c84f7e7847be0b
lmh <lmh7117@gmail.com>
2025-05-11 14:29:28 +0900

    trajectory 수정 완료

--- src/trajectory_planner.py
+++ src/trajectory_planner.py
@@ -8,9 +8,8 @@
     
     변경된 부분
     :   self.MARGINadd 추가
-    :   generate_path에서 늘린 s 만큼은 따로 계산
+    :   generate_path에서 늘린 s 만큼은 따로 계산    def visual_selected(self, selected_path):
     :   check_collision에서 가장 가까운 인덱스 찾기 추가
-    
 """
     
 # Python packages
@@ -65,21 +64,6 @@ class TrajectoryPlanner: # path planner
         self.S_MARGIN = 7 #5    # 생성한 경로 끝 추가로 경로 따라서 생성할 길이
         self.S_MARGINadd = 5
         self.collision_count = False
-
-
-    def visual_candidate_5(self, candidate_paths):
-        self.cd_path = PointCloud()
-
-        for i in range(len(candidate_paths)):
-            # print(candidate_paths[i].x[0])
-            for j in range(len(candidate_paths[i].x)):
-                p = Point32()
-                p.x = candidate_paths[i].x[j]
-                p.y = candidate_paths[i].y[j]
-                p.z = 0.0
-                self.cd_path.points.append(p)
-
-        self.candidate_pub.publish(self.cd_path)
 
     def visual_selected(self, selected_path):
         self.sl_path = PointCloud()
@@ -166,51 +150,3 @@ class TrajectoryPlanner: # path planner
         ##############################################
 
         return selected_path
-
-    # def optimal_trajectory(self, x, y, heading, obs_xy, qf=ROAD_WIDTH/2, path_num=5, path_len=3, MACARON_TREAD=2, parking=0):
-    #         # collision_count = False
-    #         if path_num == 3:
-    #             self.S_MARGIN = 3  # 3차 사전주행 값 3
-    #         else:
-    #             self.S_MARGIN = 1.8 + 5   # 예선 : 1.8, 본선 : 5 # 3차 사전주행 값 5
-
-    #         si, qi = self.glob_path.xy2sl(x, y)
-    #         self.current_s = si
-    #         self.current_q = qi
-    #         ryaw = self.glob_path.get_current_reference_yaw()
-    #         dtheta = heading - ryaw
-
-    #         if path_num == -1:
-    #             safe_candidate_paths = self.generate_path_reverse(si, qi, dtheta, path_len, 0, 1)
-    #         else:
-    #             safe_candidate_paths = self.generate_path(si, qi, dtheta, path_len, 0, 1)
-            
-    #         if path_num == 1 or path_num == -1:
-    #             if self.visual is True:
-    #                 self.visual_selected(safe_candidate_paths[0])
-    #             return safe_candidate_paths[0]
-                
-
-    #         selected_path = self.__select_optimal_trajectory(safe_candidate_paths, obs_xy,MACARON_TREAD)
-    #         if selected_path is None:
-    #             # collision_count = True
-    #             safe_candidate_paths = self.generate_path(si, qi, dtheta, path_len, qf, path_num)
-    #             ############### RVIZ 비쥬얼 코드 ##############
-    #             # if self.visual == True:
-    #             #     self.visual_candidate_5(safe_candidate_paths)
-    #             ##############################################
-    #             selected_path = self.__select_optimal_trajectory(safe_candidate_paths, obs_xy,MACARON_TREAD)
-
-    #             if selected_path is None:
-    #                 print("nothing is selected!!!!!!!!!!!!!!!!!")
-    #                 selected_path = self.__select_longest_trajectory(safe_candidate_paths,obs_xy,MACARON_TREAD)
-            
-    #         self.last_selected_path = selected_path
-    #         ############### RVIZ 비쥬얼 코드 ##############
-    #         if self.visual == True:
-    #             self.visual_selected(selected_path)
-    #             # self.max_curvature_pub(selected_path, collision_count, path_len, heading)
-    #         ##############################################
-
-    #         return selected_path
-    #         # return selected_path.x, selected_path.y
